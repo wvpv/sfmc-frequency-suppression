f535bb94885c568cd1ccdb2d6ed954ab7d3916f4
Adam Spriggs <aspriggs@degdigital.com>
2020-03-17 09:29:10 -0500

    add description, suppression whitelist

--- daily_frequency_suppression.sql
+++ daily_frequency_suppression.sql
@@ -5,6 +5,14 @@ inner join ent._Subscribers s on s.subscriberid = sas.subscriberid
 where 1=1
 and sas.sendCount > 3
 and s.EmailAddress not like '%degdigital.com'
+and not exists (
+
+    select top 1 w.*
+    from Daily_Frequency_Suppression_Whitelist w
+    where w.EmailAddress = s.EmailAddress
+
+)
 /* name: Daily_Frequency_Suppression */
+/* description: selects subscribers who are above the weekly send threshold, excluding those in the whitelist */
 /* target: Daily_Frequency_Suppression */
 /* action: overwrite */
